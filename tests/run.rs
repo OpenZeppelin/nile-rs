e792bfa09473839508cfa09c5862d8f96b2ff6ff
Eric Nordelo <eric.nordelo39@gmail.com>
2023-02-20 17:50:44 +0100

    feat: format files

--- tests/run.rs
+++ tests/run.rs
@@ -2,11 +2,7 @@ use nile_test_utils::{expected_stdout, snapbox::get_snapbox};
 
 #[test]
 fn test_run() {
-    let assert = get_snapbox()
-        .arg("run")
-        .arg("-h")
-        .assert()
-        .success();
+    let assert = get_snapbox().arg("run").arg("-h").assert().success();
 
     assert.stdout_eq(expected_stdout("run -h"));
 }
