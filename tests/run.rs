2218ac4644df23cb22df59b4f34c24e8b389f31f
Eric Nordelo <eric.nordelo39@gmail.com>
2023-02-21 14:46:00 +0100

    fix: test

--- tests/run.rs
+++ tests/run.rs
@@ -2,7 +2,13 @@ use nile_test_utils::{expected_stdout, snapbox::get_snapbox};
 
 #[test]
 fn test_run() {
-    let assert = get_snapbox().arg("run").arg("declare").assert().success();
+    let pt = assert_fs::TempDir::new().unwrap();
+    let assert = get_snapbox()
+        .current_dir(&pt)
+        .arg("run")
+        .arg("declare")
+        .assert()
+        .success();
 
     assert.stdout_eq(expected_stdout("run"));
 }
